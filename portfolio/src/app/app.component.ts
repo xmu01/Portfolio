0670371752cd6af5147f85e047aca3a442254ddd
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-03-11 17:56:12 +0100

    5th, deleted unnecessary folders

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 //import { RouterOutlet } from '@angular/router'; // Ich glaube,lösche ich am Ende, damit ich die Components nicht 2 mal einbinde. Ich habe schon <app-router> in app.component.html gelöscht. Falls ich es wieder einfügen möchte, in imports auch gucken..
 
-import { HeaderComponent } from './header/header/header.component';
-import { Section01HeroComponent } from './section01-hero/section01-hero/section01-hero.component';
-import { Section02AboutComponent } from './section02-about/section02-about/section02-about.component';
-import { Section03SkillsComponent } from './section03-skills/section03-skills/section03-skills.component';
-import { Section04PortfolioComponent } from './section04-portfolio/section04-portfolio/section04-portfolio.component';
-import { Section05ContactComponent } from './section05-contact/section05-contact/section05-contact.component';
-import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { Section01HeroComponent } from './section01-hero/section01-hero.component';
+import { Section02AboutComponent } from './section02-about/section02-about.component';
+import { Section03SkillsComponent } from './section03-skills/section03-skills.component';
+import { Section04PortfolioComponent } from './section04-portfolio/section04-portfolio.component';
+import { Section05ContactComponent } from './section05-contact/section05-contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './header/header/header.component';
-import { Section01HeroComponent } from './section01-hero/section01-hero/section01-hero.component';
-import { Section02AboutComponent } from './section02-about/section02-about/section02-about.component';
-import { Section03SkillsComponent } from './section03-skills/section03-skills/section03-skills.component';
-import { Section04PortfolioComponent } from './section04-portfolio/section04-portfolio/section04-portfolio.component';
-import { Section05ContactComponent } from './section05-contact/section05-contact/section05-contact.component';
-import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { Section01HeroComponent } from './section01-hero/section01-hero.component';
+import { Section02AboutComponent } from './section02-about/section02-about.component';
+import { Section03SkillsComponent } from './section03-skills/section03-skills.component';
+import { Section04PortfolioComponent } from './section04-portfolio/section04-portfolio.component';
+import { Section05ContactComponent } from './section05-contact/section05-contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
     { path: '', component: HeaderComponent },
