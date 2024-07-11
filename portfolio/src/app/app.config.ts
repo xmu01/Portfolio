dcf96bc0477e4916311a86220f5fc3e0667c06cd
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-07-11 15:41:53 +0200

    27 ngx translate

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -1,9 +1,39 @@
-import { ApplicationConfig } from '@angular/core';
+/*import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TranslateLoader } from '@ngx-translate/core'; // Neu importiert. Der gehört auch in app.config.ts
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync()]
+}; */
+
+
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpBackend, HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export const provideTranslation = () => ({
+defaultLanguage: 'en',
+loader: {
+provide: TranslateLoader,
+useFactory: HttpLoaderFactory,
+deps: [HttpClient],
+},
+});
+
+export function HttpLoaderFactory(http: HttpClient) {
+return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+export const appConfig: ApplicationConfig = {
+providers: [provideRouter(routes),provideAnimations(),provideHttpClient(),importProvidersFrom(HttpClientModule), importProvidersFrom([TranslateModule.forRoot(provideTranslation())
+]),],
+
 };
+
